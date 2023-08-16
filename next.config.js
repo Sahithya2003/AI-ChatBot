a02eb173d883e2a6ba19743610e615d0a045082c
Sahithya <aythihas2003@gmail.com>
2023-08-16 15:29:34 +0530

    First commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                fs: false,
+                child_process: false,
+            };
+        }
+        return config;
+    },
+};
+
